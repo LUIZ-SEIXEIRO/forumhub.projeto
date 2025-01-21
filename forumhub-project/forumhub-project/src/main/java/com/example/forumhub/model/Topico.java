ba9bf0badcac4e2fd9fee3635b667b7e968e5f83
Luiz Seixeiro <113383806+LUIZ-SEIXEIRO@users.noreply.github.com>
2025-01-20 21:52:40 -0300

    Luiz Seixeiro
    Go Live

---
+++ forumhub-project/forumhub-project/src/main/java/com/example/forumhub/model/Topico.java
@@ -0,0 +1,34 @@
+
+package com.example.forumhub.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "topicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensagem;
+
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    public Topico(String titulo, String mensagem) {
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+    }
+}
+                                    
